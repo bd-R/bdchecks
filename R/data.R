dcf77b9da6cb6d0b85a8d8c6ba9d90c9bf48f256
PoGibas <povilasgibas@gmail.com>
2018-06-03 17:34:50 +0300

    Moved test bat data to package also included yaml to data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Australian bats biodiversity data
+#' 
+#' A dataset of Australian bats (\emph{chiroptera}) (1000 records) downloaded using
+#' \code{rgbif} package.
+#' 
+#' @name dataBats
+#' @docType data
+#'
+NULL
+
+#' Data checks in YAML format
+#' 
+#' @name DCtest
+#' @docType data
+#' 
+NULL
