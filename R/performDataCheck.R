00075a8d34c7bbc3d70d12d6905954849c5c1ff9
PoGibas <povilasgibas@gmail.com>
2018-06-23 14:38:03 +0300

    Main function to perform data checks. Solved dependency part

--- R/performDataCheck.R
+++ R/performDataCheck.R
@@ -1,20 +1,69 @@
-performDataCheck <- function(data = NULL,
-                             DC = ls(pos = ("package:bdchecks"), pattern = "^DC_")) {
-    wantedDC    <- sub("^DC_", "", DC)
-    performedDC <- character(length(DC))
-    result <- list()
+#' Perform data checks
+#'
+#' `performDataCheck()` is a function for performing all available data checks
+#' on a give data set
+#'
+#' @param data Data set to perform data checks
+#' @param DCadd Character vector of names for additional data checks to be performed
+#' @param DCstand Character vector of standardize data checks
+#' @param verbose Message which data check is being performed
+#'
+#' @return Object of a dataCheckFlag class (combined result for all performed
+#' data checks)
+#' 
+#' @examples
+#' resultDataChecks <- performDataCheck(dataBats)
+#' 
+#' @export
+#' 
+performDataCheck <- function(
+    data    = NULL,
+    DCadd   = NULL,
+    verbose = TRUE,
+    DCstand = ls(pos = ("package:bdchecks"), pattern = "^DC_")) {
+
+    # All data checks to perform   
+    DCall <- unique(c(DCadd, DCstand))
+
+    # Check if all data checks exists
+    DCexists <- sapply(DCall, exists)
+    if (any(!DCexists)) {
+        warning("Following data checks don't exists (will skip them):\n", 
+                paste(DCall[!DCexists], collapse = ", "))
+        DCall <- DCall[DCexists]
+    }
+    
+    wantedDC    <- sub("^DC_", "", DCall)
+    performedDC <- character(length(DCall))
+
+    resultDC <- list()
     while(!all(wantedDC %in% performedDC)) {
-        for(i in seq_along(DC)) {
-            DCcurrent <- get(DC[[i]])
+        for(i in seq_along(DCall)) {
+
+            DCcurrent <- get(DCall[[i]])
+
             # If there are no dependencies then it's safe to run DC
             DCsafe <- is.null(DCcurrent@input$Dependency$DataChecks)
             if (!DCsafe) {
-                # Check if required dependencies are already performed
-                DCsafe <- DCcurrent@input$Dependency$DataChecks %in% performedDC
+                if (!DCcurrent@input$Dependency$DataChecks %in% wantedDC) {
+                    # If dependency is not the list then perform it
+                    DCsafe <- TRUE
+                    warning("Dependency ", 
+                            paste(DCcurrent@input$Dependency$DataChecks, collapse = ", "),
+                            " for data check ", DCcurrent@name, 
+                            " wasn't not specified to run.\n
+                            Will ignore this dependency")
+                } else {
+                    # Check if required dependencies are already performed
+                    DCsafe <- DCcurrent@input$Dependency$DataChecks %in% performedDC
+                }
             }
 
             if (DCsafe & !wantedDC[i] %in% performedDC) {
-                result[[i]] <- new("dataCheckFlag_SINGLE",
+                if (verbose) {
+                    message("Performing data check: ", DCcurrent@name)
+                }
+                resultDC[[i]] <- new("dataCheckFlag_SINGLE",
                     name   = DCcurrent@name,
                     target = DCcurrent@input$Target,
                     result = performDC(DCcurrent, data),
@@ -24,8 +73,8 @@ performDataCheck <- function(data = NULL,
         }
     }
     resultDC <- new("dataCheckFlag", 
-        DC       = as.character(lapply(result, function(x) `@`(x, name))),
-        flags    = result,
+        DC       = as.character(lapply(resultDC, function(x) `@`(x, name))),
+        flags    = resultDC,
         dataOrig = data,
         dataMod  = data)
     return(resultDC)
