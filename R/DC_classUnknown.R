eff301a8711aff41b5ffd5647a78e64197dc49ed
PoGibas <povilasgibas@gmail.com>
2018-06-25 19:41:39 +0300

    DC implementation: classUnknown - STARTED; need referenve vocabulary

---
+++ R/DC_classUnknown.R
@@ -0,0 +1,10 @@
+#'
+#' Data check classUnknown The full scientific name of the class in which the taxon is classified is within given vocabulary
+#'
+#'     This data check answers: "?" question.\cr Data check will pass if \strong{Taxonomic rank Class was interpreted correctly} and will fail if \strong{Taxonomic rank Class value cannot be interpreted}.\cr Dimension of this data check is \strong{Conformance} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{}, such data check would return \code{}.\cr Example of entries that will fail: \code{}, such data check would return \code{}.
+#' @name classUnknown
+#' @docType data
+#' @format An object of class \code{"dataCheck"}, see \code{\link{dataCheck}} for details.
+#' @references LATER
+
+NULL
