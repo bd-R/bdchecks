48787f1874d6aa256b3fd755223b8805f4aecdf2
PoGibas <povilasgibas@gmail.com>
2018-06-27 16:35:21 +0300

    DC implementation: simplified functions - removed return result from functions and replaced is.null to is.na (this is what data checkers probably had in mind)

--- R/DC_countryMismatch.R
+++ R/DC_countryMismatch.R
@@ -1,5 +1,5 @@
 #'
-#' Data check countryMismatch FOO
+#' Data check countryMismatch Check if given country match given country code.
 #'
 #'     This data check answers: "?" question.\cr Data check will pass if \strong{Country name (dwc:country) and ISO country code (dwc:countryCode) match} and will fail if \strong{Country name (dwc:country) and ISO country code (dwc:countryCode) do not match}.\cr Dimension of this data check is \strong{Consistency} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{}, such data check would return \code{}.\cr Example of entries that will fail: \code{}, such data check would return \code{}.
 #' @name countryMismatch
