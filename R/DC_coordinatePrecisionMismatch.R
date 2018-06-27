aef82dcf1fdb0c1025eb45d0a19ee5688b6d570a
PoGibas <povilasgibas@gmail.com>
2018-06-27 14:23:50 +0300

    DC implementation: coordinatesZero - FINISHED

--- R/DC_coordinatePrecisionMismatch.R
+++ R/DC_coordinatePrecisionMismatch.R
@@ -1,5 +1,5 @@
 #'
-#' Data check coordinatePrecisionMismatch Check if decimal value given in decimalLatitude and decimalLongitude does agree witha a decimal representation of the precision of the coordinates (coordinatePrecision)
+#' Data check coordinatePrecisionMismatch Check if decimal values given in decimalLatitude and decimalLongitude does agree witha a decimal representation of the precision of the coordinates (coordinatePrecision)
 #'
 #'     This data check answers: "?" question.\cr Data check will pass if \strong{The number of decimal places of latitude (dwc:decimalLatitude) and longitude (dwc:decimalLongitude) is in agreement with the supplied coordinate precision (dwc:coordinatePrecision)} and will fail if \strong{The number of decimal places of latitude (dwc:decimalLatitude) or longitude (dwc:decimalLongitude) is not in agreement with the supplied coordinate precision (dwc:coordinatePrecision)}.\cr Dimension of this data check is \strong{Consistency} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{}, such data check would return \code{}.\cr Example of entries that will fail: \code{}, such data check would return \code{}.
 #' @name coordinatePrecisionMismatch
