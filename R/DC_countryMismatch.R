0138cc265ff92f4ee8b3548f0bf5fe00629cf90c
PoGibas <povilasgibas@gmail.com>
2018-06-27 14:42:47 +0300

    DC implementation: countryMismatch - STARTED (needs description)

---
+++ R/DC_countryMismatch.R
@@ -0,0 +1,10 @@
+#'
+#' Data check countryMismatch FOO
+#'
+#'     This data check answers: "?" question.\cr Data check will pass if \strong{Country name (dwc:country) and ISO country code (dwc:countryCode) match} and will fail if \strong{Country name (dwc:country) and ISO country code (dwc:countryCode) do not match}.\cr Dimension of this data check is \strong{Consistency} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{}, such data check would return \code{}.\cr Example of entries that will fail: \code{}, such data check would return \code{}.
+#' @name countryMismatch
+#' @docType data
+#' @format An object of class \code{"dataCheck"}, see \code{\link{dataCheck}} for details.
+#' @references LATER
+
+NULL
