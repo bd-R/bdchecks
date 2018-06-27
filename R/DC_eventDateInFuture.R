3e4394bcd7842b781cdcd8ef16d7eeceb4b646c1
PoGibas <povilasgibas@gmail.com>
2018-06-27 17:32:27 +0300

    DC implementation: eventDateInFuture - FINISHED

---
+++ R/DC_eventDateInFuture.R
@@ -0,0 +1,10 @@
+#'
+#' Data check eventDateInFuture Check if event date is not in the future
+#'
+#'     This data check answers: "Is event date not in the future??" question.\cr Data check will pass if \strong{dwc:eventDate is valid} and will fail if \strong{dwc:eventDate is in the future}.\cr Dimension of this data check is \strong{Conformance} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{}, such data check would return \code{}.\cr Example of entries that will fail: \code{}, such data check would return \code{}.
+#' @name eventDateInFuture
+#' @docType data
+#' @format An object of class \code{"dataCheck"}, see \code{\link{dataCheck}} for details.
+#' @references LATER
+
+NULL
