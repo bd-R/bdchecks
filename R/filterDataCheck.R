261e57e317a6bee0d5e1e237658de6781dba937a
PoGibas <povilasgibas@gmail.com>
2018-06-30 00:09:43 +0300

    Also no point to keep filtering function as generic

--- R/filterDataCheck.R
+++ R/filterDataCheck.R
@@ -1,30 +1,24 @@
-setGeneric("filterDataCheck", function(DCresult, DCfilts = NULL) {
-    standardGeneric("filterDataCheck")
-})
-setMethod("filterDataCheck", "dataCheckFlag",
-    function(DCresult, DCfilts) {
-
-        # Using for loop as this is light function
-        # And I want to keep it clear and simple
-        idx <- c()
-        names <- sapply(DCresult@flags, function(x) `@`(x, name))
-        targets <- sapply(DCresult@flags, function(x) `@`(x, target))
-        for(i in seq_along(DCfilts$name)) {
-            foo <- DCresult@flags[names == DCfilts$name[i] & targets == DCfilts$target[i]][[1]]@result
-            bar <- c()
-            # Remove passed
-            if (grepl("P", DCfilts$filter[i], ignore.case = TRUE)) {
-                bar <- c(bar, which(foo))
-            }
-            if (grepl("F", DCfilts$filter[i], ignore.case = TRUE)) {
-                bar <- c(bar, which(!foo))
-            }
-            if (grepl("M", DCfilts$filter[i], ignore.case = TRUE)) {
-                bar <- c(bar, which(is.na(foo)))
-            }
-            idx <- c(idx, bar)
+filterDataCheck <- function(DCresult, DCfilts = NULL) {
+    # Using for loop as this is light function
+    # And I want to keep it clear and simple
+    idx <- c()
+    names <- sapply(DCresult@flags, function(x) `@`(x, name))
+    targets <- sapply(DCresult@flags, function(x) `@`(x, target))
+    for(i in seq_along(DCfilts$name)) {
+        foo <- DCresult@flags[names == DCfilts$name[i] & targets == DCfilts$target[i]][[1]]@result
+        bar <- c()
+        # Remove passed
+        if (grepl("P", DCfilts$filter[i], ignore.case = TRUE)) {
+            bar <- c(bar, which(foo))
         }
-        idx <- unique(idx)
-        DCresult@dataMod[-idx, ]
-
-})
+        if (grepl("F", DCfilts$filter[i], ignore.case = TRUE)) {
+            bar <- c(bar, which(!foo))
+        }
+        if (grepl("M", DCfilts$filter[i], ignore.case = TRUE)) {
+            bar <- c(bar, which(is.na(foo)))
+        }
+        idx <- c(idx, bar)
+    }
+    idx <- unique(idx)
+    DCresult@dataMod[-idx, ]
+}
