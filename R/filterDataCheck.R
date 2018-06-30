9218eac6527b6b6912934d6c6168e4ed8731da63
PoGibas <povilasgibas@gmail.com>
2018-06-30 10:00:49 +0300

    Stop user from filtering all three options (as we're getting 0 roes)

--- R/filterDataCheck.R
+++ R/filterDataCheck.R
@@ -1,23 +1,25 @@
-filterDataCheck <- function(DCresult, DCfilts = NULL) {
-    # Using for loop as this is light function
-    # And I want to keep it clear and simple
+filterDataCheck <- function(DCresult, DCfilts) {
     idx <- c()
     names <- sapply(DCresult@flags, function(x) `@`(x, name))
     targets <- sapply(DCresult@flags, function(x) `@`(x, target))
     for(i in seq_along(DCfilts$name)) {
-        foo <- DCresult@flags[names == DCfilts$name[i] & targets == DCfilts$target[i]][[1]]@result
-        bar <- c()
-        # Remove passed
-        if (grepl("P", DCfilts$filter[i], ignore.case = TRUE)) {
-            bar <- c(bar, which(foo))
+        if (nchar(DCfilts$filter[i]) < 3) {
+            foo <- DCresult@flags[names == DCfilts$name[i] & targets == DCfilts$target[i]][[1]]@result
+            bar <- c()
+            # Remove passed
+            if (grepl("P", DCfilts$filter[i], ignore.case = TRUE)) {
+                bar <- c(bar, which(foo))
+            }
+            if (grepl("F", DCfilts$filter[i], ignore.case = TRUE)) {
+                bar <- c(bar, which(!foo))
+            }
+            if (grepl("M", DCfilts$filter[i], ignore.case = TRUE)) {
+                bar <- c(bar, which(is.na(foo)))
+            }
+            idx <- c(idx, bar)
+        } else {
+            warning("Can't filter all three")
         }
-        if (grepl("F", DCfilts$filter[i], ignore.case = TRUE)) {
-            bar <- c(bar, which(!foo))
-        }
-        if (grepl("M", DCfilts$filter[i], ignore.case = TRUE)) {
-            bar <- c(bar, which(is.na(foo)))
-        }
-        idx <- c(idx, bar)
     }
     idx <- unique(idx)
     DCresult@dataMod[-idx, ]
