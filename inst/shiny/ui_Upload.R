316baeaf73f82e441ad5b3fabd1631410a007b8b
PoGibas <povilasgibas@gmail.com>
2018-07-20 15:32:40 +0300

    Added collapse panels for data upload: upload options code taken from bdclean

--- inst/shiny/ui_Upload.R
+++ inst/shiny/ui_Upload.R
@@ -2,11 +2,48 @@ fluidPage(
     titlePanel("Upload Data"),
     sidebarLayout(
         sidebarPanel(
-            fileInput("pathInput", "Choose File"),
-            actionButton("acceptFile", "Accepth File")
-    ),
-    mainPanel(
-        DT::dataTableOutput('contents')
+            bsCollapse(multiple = FALSE, open = "Upload Local File",
+                       bsCollapsePanel("Upload Local File", 
+                                       fileInput("pathInput",
+                                                 "Choose a csv file",
+                                                 multiple = FALSE,
+                                                 c("text/csv",
+                                                   "text/comma-separated-values,text/plain",
+                                                   ".csv")
+                                       ), style = "info"
+
+                       ),
+                       bsCollapsePanel("Query Data From a Database",
+                                       textInput("scientificName",
+                                                 h3("Scientific Name:"),
+                                                 "Puma concolor"),
+                                       sliderInput("recordSize",
+                                                   h3("Record Size:"),
+                                                   500,
+                                                   min = 0, max = 50000),
+                                       checkboxGroupInput("queryDB",
+                                                          h3("Online Database:"),
+                                                          list("GBIF" = 'gbif',
+                                                               "Vertnet" = 'vertnet',
+                                                               "Bison" = 3,
+                                                               "Inat" = 4,
+                                                               "eBird" = 5,
+                                                               "Ecoengine" = 6,
+                                                               "Vertnet" = 7),
+                                                          "gbif"
+                                       ),
+                                       br(),
+                                       div(id = "queryDatabaseDiv",
+                                           class = "activeButton",
+                                           actionButton("queryDatabase", 
+                                                        "Query Database", 
+                                                        icon("download"))), style = "success"
+                       )
+            )
+        ),
+        mainPanel(
+            actionButton("acceptFile", "Accept Dataset"),
+            DT::dataTableOutput("contents")
+        )
     )
-  )
 )
