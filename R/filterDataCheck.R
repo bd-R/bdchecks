1e4a9e73c0259948275d6e5c960fd9b55b97c427
PoGibas <povilasgibas@gmail.com>
2018-08-21 20:29:05 +0300

    V 0.0.2 Refactored functions to filter data checks

--- R/filterDataCheck.R
+++ R/filterDataCheck.R
@@ -1,3 +1,42 @@
+#' Filter Data Checks
+#'
+#' `generateDCfilts()` is a function that generates vector for filtering data 
+#' checks result table according to `DT::datatable()` `selectedCells` object.
+#' 
+#' @param DCresultSummary Summary table for a dataCheckFlag class
+#' (must be filterable in `DT`)
+#' @param selectedCells Cells selected in `DT::datatable`
+#' @param filters Vector that contains names for passed, failed and missing 
+#' data checks
+#' 
+#' @return A list that contains name of a data checks, it's target and 
+#' filtering status
+#'
+generateDCfilts <- function(DCresultSummary, selectedCells, filters = c("P", "F", "M")) {
+    result <- lapply(unique(selectedCells[, 1]), function(i) {
+        DCcurrent <- DCresultSummary[i, ]
+        foo <- unique(selectedCells[selectedCells[, 1] == i, 2] - 1)
+        list(name = DCcurrent[, 1],
+             target = DCcurrent[, 2],
+             filter = paste(filters[foo[foo > 0]], collapse = ""))
+    })
+    result <- list(name = sapply(result, "[[", 1),
+                   target = sapply(result, "[[", 2),
+                   filter = sapply(result, "[[", 3))
+    return(result)
+}
+
+#' Filter Data Checks
+#'
+#' `filterDataCheck()` is a function that filters data check result according
+#'  to filtering vector.
+#' 
+#' @param DCresult Object of a dataCheckFlag generated with `perforDataCheck()`
+#' @param DCfilts A list containing filtering targets and status generated with 
+#' `generateDCfilts()`
+#' 
+#' @return A data.frame that is filtered according to given vector
+#' 
 filterDataCheck <- function(DCresult, DCfilts) {
     idx <- c()
     names <- sapply(DCresult@flags, function(x) `@`(x, name))
@@ -22,19 +61,5 @@ filterDataCheck <- function(DCresult, DCfilts) {
         }
     }
     idx <- unique(idx)
-    DCresult@dataMod[-idx, ]
-}
-
-generateDCfilts <- function(DCresultSummary, selectedCells, filters = c("P", "F", "M")) {
-    result <- lapply(unique(selectedCells[, 1]), function(i) {
-        DCcurrent <- DCresultSummary[i, ]
-        foo <- unique(selectedCells[selectedCells[, 1] == i, 2] - 1)
-        list(name = DCcurrent[, 1],
-             target = DCcurrent[, 2],
-             filter = paste(filters[foo[foo > 0]], collapse = ""))
-    })
-
-    list(name = sapply(result, "[[", 1),
-         target = sapply(result, "[[", 2),
-         filter = sapply(result, "[[", 3))
+    return(DCresult@dataMod[-idx, ])
 }
