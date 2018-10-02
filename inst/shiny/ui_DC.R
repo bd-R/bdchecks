ddc935d5256de9435ef4ead18f710f85749d7709
PoGibas <povilasgibas@gmail.com>
2018-10-02 20:29:59 +0300

    v 0.0.21 Improved ui for udata checks part

--- inst/shiny/ui_DC.R
+++ inst/shiny/ui_DC.R
@@ -3,17 +3,30 @@ fluidPage(
     conditionalPanel("!input.acceptFile", 
                      h2("Upload and Accept Dataset")),
     conditionalPanel("input.acceptFile",
-        radioButtons("DCgroupsAvailable", 
-                     "Select category to group data checks:",
-                     c("Darwin Core Class" = "DarwinCoreClass",
-                       "Dimension" = "Dimension",
-                       "Warning Type" = "Warning",
-                       "Output Type" = "Output",
-                       "Severity" = "Severity"),
-                     "DarwinCoreClass"),
-        actionButton("selectDC", "Select Data Checks"),
         fluidRow(
-            uiOutput("DCgroupsCheckBox")),
+            column(width = 2,
+                radioButtons("DCgroupsAvailable", 
+                             "Select category to group data checks:",
+                             c("Darwin Core Class" = "DarwinCoreClass",
+                               "Dimension" = "Dimension",
+                               "Warning Type" = "Warning",
+                               "Output Type" = "Output",
+                               "Severity" = "Severity"),
+                               "DarwinCoreClass")
+            ),
+            column(width = 3,
+                actionButton("selectDC", "Accept Data Checks",
+                                   icon = icon("check"), 
+                                   width = 210,
+                                   style = "color: #000000; background-color: #71a879; border-color: #091520;
+                                            padding:10px; font-size:120%")
+            ),
+            column(12, style = "margin-bottom:10px; border-bottom:2px solid")
+        ),
+        br(), br(),
+        fluidRow(
+            uiOutput("DCgroupsCheckBox")
+        ),
         uiOutput("DCcheckBoxHover")
     )
 )
