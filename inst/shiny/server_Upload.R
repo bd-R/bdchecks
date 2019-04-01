03437a619eb8cef2482b300aab01e17e45cfe1ed
PoGibas <povilasgibas@gmail.com>
2019-04-01 19:27:38 +0200

    v0.1.13 Added if condition to skip following analysis if there's no input data

--- inst/shiny/server_Upload.R
+++ inst/shiny/server_Upload.R
@@ -104,26 +104,26 @@ shiny::observeEvent(input$query_database, {
 })
 
 shiny::observe({
-  darwinizer <- bdDwC::darwinize_names(
-    rv$data_original,
-    bdDwC:::data_darwin_cloud$data
-  )
-  fixed <- darwinizer[darwinizer$match_type == "Darwinized", ]
-  if (nrow(fixed) > 0) {
-    rv$data_original <- bdDwC::rename_user_data(rv$data_original, darwinizer)
-    shiny::showNotification(paste(
-      "Converted Columns:",
-      paste(
-        paste(fixed[, 1], collapse = ", "),
-        paste(fixed[, 2], collapse = ", "),
-        sep = " -> "
-      )
-    ),
-    duration = 7
-    )
-  }
-  data_loaded_task(rv$data_original)
   if (nrow(rv$data_original) > 0) {
+    darwinizer <- bdDwC::darwinize_names(
+      rv$data_original,
+      bdDwC:::data_darwin_cloud$data
+    )
+    fixed <- darwinizer[darwinizer$match_type == "Darwinized", ]
+    if (nrow(fixed) > 0) {
+      rv$data_original <- bdDwC::rename_user_data(rv$data_original, darwinizer)
+      shiny::showNotification(paste(
+        "Converted Columns:",
+        paste(
+          paste(fixed[, 1], collapse = ", "),
+          paste(fixed[, 2], collapse = ", "),
+          sep = " -> "
+        )
+      ),
+      duration = 7
+      )
+    }
+    data_loaded_task(rv$data_original)
     shinyjs::enable("accept_file")
   }
 })
