cc67ac37324fea3b2d30a0b7c1b910a389c8c53f
PoGibas <povilasgibas@gmail.com>
2019-06-12 21:03:51 +0300

    Adjusted R code to export roxygen comments; no data file anymore

--- R/class_export.R
+++ R/class_export.R
@@ -4,47 +4,24 @@
 #' rda and/or roxygen2 file.
 #'
 #' @param path_yaml  Path to a YAML file.
-#' @param export_rda Should function export data check to a .rda file.
-#' @param export_rox Should function create a documentation in .R for roxygen2.
-#' @param path_rda   Path for .rda file.
-#' @param path_rox   Path for .R (roxygen2) file.
-#' @param id_rda     ID in the beginning of file for .rda file
-#' @param id_rox     ID in the beginning of file for .R file
+#' @param path_rd   Path for .R (roxygen2) file.
+#' @param id_rd     ID in the beginning of file for .R file
 #'                  (helps to differentiate other functions from data checks).
 #'
 #' @importFrom yaml yaml.load_file
 #'
-dc_export <- function(path_yaml = "./inst/extdata/dataChecks.yaml",
-                      export_rda = TRUE,
-                      export_rox = TRUE,
-                      path_rda = "./data/",
-                      path_rox = "./R/",
-                      id_rda = "DC_",
-                      id_rox = "DC_") {
+datacheck_info_export <- function(
+  path_yaml = "./inst/extdata/dataChecks.yaml",
+  path_rd = "./R/",
+  id_rd = "documentation_"
+) {
 
-  # Load YAML file with data checks
   DCyaml <- yaml::yaml.load_file(path_yaml)
-
-  # Transform DC from YAML to DC class in R
-  # Using loop and assign as base::save wants exact object name
-  # !!! TEST FOR BAD YAML
   for (x in DCyaml) {
-    if (export_rda) {
-      # Create DC object as save according to DC name
-      DC <- dc_main_create(x)
-      assign(paste0(id_rox, x$name), DC)
-      # Save DC object as rda
-      save(
-        list = paste0(id_rox, x$name),
-        file = paste0(path_rda, id_rda, x$name, ".rda")
-      )
-    }
-    if (export_rox) {
-      writeLines(
-        roxygen_comment_generate(DC),
-        paste0(path_rox, id_rox, x$name, ".R")
-      )
-    }
+    writeLines(
+      roxygen_comment_generate(x),
+      paste0(path_rd, id_rd, x$name, ".R")
+    )
   }
   invisible()
 }
@@ -69,7 +46,6 @@ roxygen_comment_generate <- function(DC) {
     "#'",
     "longDesc",
     "#' @name",
-    "#' @docType data",
     paste(
       "#' @format An object of class \\code{\"dataCheck\"},",
       "see \\code{\\link{dataCheck}} for details."
@@ -88,56 +64,100 @@ roxygen_comment_generate <- function(DC) {
   # Add short description
   skeleton <- sub(
     "shortDesc",
-    paste("#' Data check", DC@name, DC@meta@description$Main),
+    paste("#' Data check", DC$name, DC$meta$description$Main),
     skeleton
   )
 
   # Add long description
   skeleton <- sub(
     "longDesc", paste0(
-      "#'     This data check answers: \"", DC@meta@description$InputQuestion,
+      "#'     This data check answers: \"", DC$meta$description$InputQuestion,
       "?\" question.",
       "\\\\cr Data check will pass if \\\\strong{",
-      DC@meta@description$Example$Pass, "}",
+      DC$meta$description$Example$Pass, "}",
       " and will fail if \\\\strong{",
-      DC@meta@description$Example$Fail, "}.",
+      DC$meta$description$Example$Fail, "}.",
       "\\\\cr Dimension of this data check is \\\\strong{",
-      DC@meta@flags$Dimension, "}",
+      DC$meta$flags$Dimension, "}",
       " and it's flagging type is: \\\\strong{FLAG}",
       "\\\\cr Example of entries that will pass: \\\\code{",
-      DC@meta@description$Example$InputPass, "},",
+      DC$meta$description$Example$InputPass, "},",
       " such data check would return \\\\code{",
-      DC@meta@description$Example$OutputPass, "}.",
+      DC$meta$description$Example$OutputPass, "}.",
       "\\\\cr Example of entries that will fail: \\\\code{",
-      DC@meta@description$Example$InputFail, "},",
+      DC$meta$description$Example$InputFail, "},",
       " such data check would return \\\\code{",
-      DC@meta@description$Example$OutputFail, "}."
+      DC$meta$description$Example$OutputFail, "}."
     ),
     skeleton
   )
   # Add additional fiels for the bdclean
-  skeleton <- sub("EXAMPLE@name", paste0("DC_", DC@name), skeleton)
+  skeleton <- sub("EXAMPLE@name", paste0("DC_", DC$name), skeleton)
 
   # Add name
-  skeleton <- sub("@name", paste("@name", paste0("DC_", DC@name)), skeleton)
+  skeleton <- sub("@name", paste("@name", paste0("DC_", DC$name)), skeleton)
   # Add keywords
   skeleton <- c(
     skeleton,
-    ifelse(!is.null(DC@meta@description$keywords),
-      paste("#' @keywords", DC@meta@description$keywords),
+    ifelse(!is.null(DC$meta$description$keywords),
+      paste("#' @keywords", DC$meta$description$keywords),
       ""
     )
   )
 
-  skeleton <- sub("FIELDPASS", DC@meta@description$Example$Pass, skeleton)
-  skeleton <- sub("FIELDFAIL", DC@meta@description$Example$Fail, skeleton)
-  skeleton <- sub("FIELDTARGET", DC@input$Target, skeleton)
+  skeleton <- sub("FIELDPASS", DC$meta$Description$Example$Pass, skeleton)
+  skeleton <- sub("FIELDFAIL", DC$meta$Description$Example$Fail, skeleton)
+  skeleton <- sub("FIELDTARGET", DC$Input$Target, skeleton)
   skeleton <- sub(
     "FIELDCATERGORY",
-    DC@meta@description$DarwinCoreClass,
+    DC$meta$Description$DarwinCoreClass,
     skeleton
   )
 
   body <- c("#'", skeleton, "NULL")
   return(body)
+}
+
+#' Create a data check object
+#'
+#' Create a data check object from a given YAML file
+#'
+#' @param yaml Data check entry as a list (originally imported as a
+#' YAML file)
+#'
+#' @importFrom methods new
+#'
+#' @return Data check object
+#'
+#' @export
+#'
+dc_main_create <- function(yaml) {
+  res <- methods::new("dataCheck",
+    name = yaml$name,
+    meta = dc_meta_create(yaml$meta),
+    input = yaml$Input,
+    func = parse(text = yaml$Functionality)
+  )
+  return(res)
+}
+
+#' Create a data check metadata object
+#'
+#' Create a data check metadata object from a given slot in a data check list
+#' object
+#'
+#' @param meta Data check metadata entry as a list.
+#'
+#' @importFrom methods new
+#'
+#' @return Data check metadata object
+#'
+dc_meta_create <- function(meta) {
+  res <- methods::new("dataCheckMeta",
+    description = meta$Description,
+    flags = meta$Flags,
+    pseudocode = meta$Pseudocode,
+    source = meta$Source
+  )
+  return(res)
 }
