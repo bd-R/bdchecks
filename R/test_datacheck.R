893e5e86ecbec93f9e8dd106039d8bc0e8e105d7
PoGibas <povilasgibas@gmail.com>
2019-07-19 18:06:41 +0300

    v0.1.16 Added a function to load and create test data

---
+++ R/test_datacheck.R
@@ -0,0 +1,34 @@
+#' Create a dataset to test datachecks
+#'
+#' Loads, parses and outputs datasets to test data checks
+#' 
+#' @param path_datatest String that specifies path to test data in yaml format
+#'
+#' @return A list of data.frames that contain target column, test type and
+#' expected result 
+#' 
+#' @importFrom magrittr "%>%"
+#' @importFrom yaml read_yaml
+#'
+#' @examples
+#' data_test <- create_testdata()
+#' 
+#' @export
+#'
+create_testdata <- function(
+  path_datatest = system.file("extdata/data_test.yaml", package = "bdchecks")
+) { 
+  d <- yaml::read_yaml(path_datatest)
+  data_test <- list()
+  for (i in seq_along(d)) {
+    data_current <- d[[i]]$data
+    stopifnot(!is.null(data_current))
+    stopifnot(all(c("type", "expected") %in% data_current[[1]]))
+    data_test[[i]] <- lapply(data_current[-1], as.character) %>%
+      do.call(rbind, .) %>%
+      data.frame(stringsAsFactors = FALSE)
+    colnames(data_test[[i]]) <- data_current[[1]]
+  }
+  names(data_test) <- names(d)
+  return(data_test)
+}
