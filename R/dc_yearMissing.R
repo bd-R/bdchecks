aea3a2607756c37792cb333914067d278d13deeb
PoGibas <povilasgibas@gmail.com>
2019-07-20 11:21:22 +0300

    Improved yearMissing - returns missing values

--- R/dc_yearMissing.R
+++ R/dc_yearMissing.R
@@ -3,13 +3,16 @@
 #' @param TARGET a vector of year information
 #' 
 #' @importFrom magrittr "%>%"
-#' 
+#'
 dc_yearMissing <- function(TARGET) {
   result <- TARGET %>%
     trimws() %>% # Trim extra whitespace
     as.numeric() # This is main criteria - value must be turned numeric
   # Year must be possitive and probably contain for digits
   result <- result > 0 & nchar(result) == 4
+  # Turn failed values to FALSE
   result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
   return(result)
 }
