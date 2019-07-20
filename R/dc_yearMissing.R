b042b6e5e4a880f3ad4dc6489448958f4cec43ed
PoGibas <povilasgibas@gmail.com>
2019-07-20 10:15:23 +0300

    yearMissing check - added more tests, improved function

--- R/dc_yearMissing.R
+++ R/dc_yearMissing.R
@@ -8,6 +8,7 @@ dc_yearMissing <- function(TARGET) {
   result <- TARGET %>%
     trimws() %>% # Trim extra whitespace
     as.numeric() # This is main criteria - value must be turned numeric
+  # Year must be possitive and probably contain for digits
   result <- result > 0 & nchar(result) == 4
   result[is.na(result)] <- FALSE
   return(result)
