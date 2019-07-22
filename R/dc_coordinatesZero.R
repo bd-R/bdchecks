096db17398a642eb26d0384157cf031e9be0b9ad
PoGibas <povilasgibas@gmail.com>
2019-07-22 14:26:31 +0300

    coordinatesZero  check - added more tests, improved function

--- R/dc_coordinatesZero.R
+++ R/dc_coordinatesZero.R
@@ -1,6 +1,21 @@
 #' @rdname dc_coordinatesZero
-#' @param TARGET a vector to perform data check
+#' 
+#' @param TARGET a vector of event date information
+#' @param date_format a string for full date format
+#' 
+#' @importFrom magrittr "%>%"
 #' 
 dc_coordinatesZero <- function(TARGET) {
   TARGET != 0
+  result <- TARGET %>%
+    trimws() %>% # Trim extra whitespace
+    as.numeric() # Value must be turned numeric
+  # Main part - check if value is equal to one
+  result <- result == 1
+
+  # Turn failed values to FALSE
+  result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
+  return(result)
 }
--- R/dc_dateNull.R
+++ R/dc_dateNull.R
@@ -1,6 +1,24 @@
-#' @rdname dc_dateNull
-#' @param TARGET a vector to perform data check
+#' @rdname dc_eventDateInFuture
 #' 
-dc_dateNull <- function(TARGET) {
-  !is.na(TARGET)
+#' @param TARGET a vector of event date information
+#' @param date_format a string for full date format
+#' 
+#' @importFrom magrittr "%>%"
+#' 
+dc_dateNull <- function(TARGET, date_format = "%Y-%m-%d") {
+  result <- TARGET %>%
+    trimws() %>% # Trim extra whitespace
+    gsub("\\.", "-", .) # Turn wrong seprators into relevant -
+
+  # Try to check if only year is present
+  result_year <- dc_yearMissing(ifelse(nchar(result) == 4, result, NA))
+  # Try to check if full date is present
+  result_full <- !is.na(as.Date(result, format = date_format))
+
+  result <- result_full | result_year
+  # Turn failed values to FALSE
+  result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
+  return(result)
 }
