ff864e21eee842b11b096e94fdef9c12c04f49b8
PoGibas <povilasgibas@gmail.com>
2019-07-23 09:06:37 +0300

    uncertaintyRangeMismatchh check - added more tests, improved function

--- R/dc_uncertaintyRangeMismatch.R
+++ R/dc_uncertaintyRangeMismatch.R
@@ -1,6 +1,20 @@
 #' @rdname dc_uncertaintyRangeMismatch
-#' @param TARGET a vector to perform data check
+#' 
+#' @param TARGET a vector of uncertainty
+#' 
+#' @importFrom magrittr "%>%"
 #' 
 dc_uncertaintyRangeMismatch <- function(TARGET) {
-  TARGET %% 1 == 0 & TARGET > 0
+  result <- TARGET %>%
+    trimws() %>% # Trim extra whitespace
+    gsub(" ", "", .) %>% # Remove possible spaces
+    as.numeric() # This is main criteria - value must be turned numeric
+  # Precesion must be an integer greater than 0
+  result <- result %% 1 == 0 & result > 0
+
+  # Turn failed values to FALSE
+  result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
+  return(result)
 }
