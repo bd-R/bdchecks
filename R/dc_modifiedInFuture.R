b8d8f48f3bca92af9c6e767ce3b856703e3f5c0b
PoGibas <povilasgibas@gmail.com>
2019-07-23 09:54:53 +0300

    modifiedInFuture check - added more tests, improved function

--- R/dc_modifiedInFuture.R
+++ R/dc_modifiedInFuture.R
@@ -1,6 +1,20 @@
 #' @rdname dc_modifiedInFuture
-#' @param TARGET a vector to perform data check
 #' 
-dc_modifiedInFuture <- function(TARGET) {
-  as.Date(ifelse(TARGET == "", NA, TARGET), origin = "1970-01-01") <= Sys.Date()
+#' @param TARGET a vector of a date
+#' @param date_format a string for full date format
+#' 
+#' @importFrom magrittr "%>%"
+#' 
+dc_modifiedInFuture <- function(TARGET, date_format = "%Y-%m-%d") {
+
+  result <- TARGET %>%
+    trimws() %>% # Trim extra whitespace
+    gsub("\\.", "-", .) # Turn wrong seprators into relevant -
+  result <- as.Date(result, origin = "1970-01-01") <= Sys.Date()
+
+  # Turn failed values to FALSE
+  result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
+  return(result)
 }
