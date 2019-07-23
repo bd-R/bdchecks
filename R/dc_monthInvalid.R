96d1aa63a917950d87f0a4e08fbdc73f10459cd7
PoGibas <povilasgibas@gmail.com>
2019-07-23 09:33:18 +0300

    monthInvalid check - added more tests, improved function

--- R/dc_monthInvalid.R
+++ R/dc_monthInvalid.R
@@ -1,6 +1,19 @@
 #' @rdname dc_monthInvalid
-#' @param TARGET a vector to perform data check
 #' 
+#' @param TARGET a vector of a month
+#' 
+#' @importFrom magrittr "%>%"
 dc_monthInvalid <- function(TARGET) {
-  TARGET %% 1 == 0 & TARGET >= 1 & TARGET <= 12
+  result <- TARGET %>%
+    trimws() %>% # Trim extra whitespace
+    gsub(" ", "", .) %>% # Remove possible spaces
+    as.numeric() # This is main criteria - value must be turned numeric
+  # Must must be an integer between 1 and 12
+  result <- result %% 1 == 0 & result >= 1 & result <= 12
+
+  # Turn failed values to FALSE
+  result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
+  return(result)
 }
