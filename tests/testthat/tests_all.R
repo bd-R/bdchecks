4142951697ecbda3a3ffd114107978307ce016f1
PoGibas <povilasgibas@gmail.com>
2019-07-23 21:47:50 +0300

    Added data check test report to travis

--- tests/testthat/tests_all.R
+++ tests/testthat/tests_all.R
@@ -1,6 +1,52 @@
 library(bdchecks)
 
 # Test summary functions
+context("Data Checks")
+# test perform_dc
+# performs data checks on a given data set
+test_that("perform_dc", {
+  # No data set provided will give warnings and null
+  result <- expect_warning(perform_dc())
+  expect_true(is.null(result))
+  result <- expect_warning(perform_dc(data_bats))
+  # we should have at least 18 performed DCs
+  expect_gt(length(result@DC), 10)
+  # No arguments provided
+  expect_warning(perform_dc())
+  expect_warning(perform_dc(mtcars, "monthInvalid"))
+  # This will depend on data version
+  expect_equal(sum(perform_dc(data_bats, "monthInvalid")@flags[[1]]@result), 1e3)
+})
+
+# Test data checks
+context("Test Data Checks")
+test_that("perform_test_dc", {
+  result <- expect_warning(perform_test_dc())
+  foo <- function(result) {
+      res <- list()
+      for (i in seq_along(result)) {
+        d <- result[[i]]
+        res[[i]] <- d[d$expected != d$observed, ]
+        colnames(res[[i]])[1] <- "TARGET"
+        if (nrow(res[[i]]) > 0) {
+          res[[i]] <- data.frame(dc = names(result)[i], res[[i]]) 
+          rownames(res[[i]]) <- NULL
+        }
+      }
+      res <- do.call(rbind, res)
+      if (nrow(res) > 0) {
+        print(
+          knitr::kable(res, "rst")
+        )
+        FALSE
+      } else {
+        TRUE
+      }
+    }
+  expect_true(foo(result), label = "Tests for data check")
+})
+
+# Test summary functions
 context("Summary Functions")
 # test dc_summary
 # outputs summary table for passed/failed data checks
@@ -10,41 +56,15 @@ test_that("summary_dc", {
   result <- expect_warning(perform_dc(data_bats))
   # Check if output is valid
   expect_s4_class(result, "DataCheckFlagSet")
-
   # Summary output 1
   foo <- summary_dc(result, fancy = FALSE, filtering_dt = FALSE)
   expect_s3_class(foo, "data.frame")
   # expect_equal(nrow(foo), 22)
   expect_equal(ncol(foo), 5)
-
   foo <- summary_dc(result, fancy = FALSE, filtering_dt = TRUE)
   expect_s3_class(foo, "data.frame")
   expect_match(colnames(foo)[3:5], "%")
-
   # Summary output 2
   foo <- summary_dc(result)
   expect_s3_class(foo, "knitr_kable")
 })
-
-# Test summary functions
-context("Data Checks")
-# test perform_dc
-# performs data checks on a given data set
-test_that("perform_dc", {
-  # No data set provided will give warnings and null
-  result <- expect_warning(perform_dc())
-  expect_true(is.null(result))
-
-  result <- expect_warning(perform_dc(data_bats))
-  # we should have at least 18 performed DCs
-  expect_gt(length(result@DC), 10)
-
-  # performs one data check on a given data set
-  test_that("perform_dc", {
-  # No arguments provided
-  expect_warning(perform_dc())
-  expect_warning(perform_dc(mtcars, "monthInvalid"))
-  # This will depend on data version
-  expect_equal(sum(perform_dc(data_bats, "monthInvalid")@flags[[1]]@result), 1e3)
-})
-
