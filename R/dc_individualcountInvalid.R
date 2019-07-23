8a219b1d103e8a82ace96d8a75fb1e7f15fbc3db
PoGibas <povilasgibas@gmail.com>
2019-07-23 09:12:10 +0300

    Type in check name - individualCountInvalid

--- R/dc_individualcountInvalid.R
+++ R/dc_individualcountInvalid.R
@@ -1,6 +1,20 @@
 #' @rdname dc_individualcountInvalid
-#' @param TARGET a vector to perform data check
 #' 
-dc_individualcountInvalid <- function(TARGET) {
-  TARGET %% 1 == 0 & TARGET >= 0
+#' @param TARGET a vector of number of individuals
+#' 
+#' @importFrom magrittr "%>%"
+#' 
+dc_individualCountInvalid <- function(TARGET) {
+  result <- TARGET %>%
+    trimws() %>% # Trim extra whitespace
+    gsub(" ", "", .) %>% # Remove possible spaces
+    as.numeric() # This is main criteria - value must be turned numeric
+  # Number of individuals must be an integer greater than 0
+  result <- result %% 1 == 0 & result > 0
+
+  # Turn failed values to FALSE
+  result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
+  return(result)
 }
