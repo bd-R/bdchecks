9ab052e8c0459d85254a03e0199f307cf9ce0322
PoGibas <povilasgibas@gmail.com>
2019-07-23 09:29:51 +0300

    dayInvalid check - added more tests, improved function

--- R/dc_dayInvalid.R
+++ R/dc_dayInvalid.R
@@ -1,6 +1,19 @@
 #' @rdname dc_dayInvalid
-#' @param TARGET a vector to perform data check
 #' 
+#' @param TARGET a vector of a day
+#' 
+#' @importFrom magrittr "%>%"
 dc_dayInvalid <- function(TARGET) {
-  TARGET %% 1 == 0 & TARGET >= 1 & TARGET <= 31
+  result <- TARGET %>%
+    trimws() %>% # Trim extra whitespace
+    gsub(" ", "", .) %>% # Remove possible spaces
+    as.numeric() # This is main criteria - value must be turned numeric
+  # Day must be an integer between 1 and 31
+  result <- result %% 1 == 0 & result >= 1 & result <= 31
+
+  # Turn failed values to FALSE
+  result[is.na(result)] <- FALSE
+  # Get original missing values
+  result[is.na(TARGET) | TARGET == ""] <- NA
+  return(result)
 }
