ac6738e9800bfd0710d246486e3a6b24ceb5b5a9
PoGibas <povilasgibas@gmail.com>
2019-08-27 19:51:52 +0300

    Added validation_scientificname_empty data check

--- R/dc_validation_taxon_empty.R
+++ R/dc_validation_taxon_empty.R
@@ -3,7 +3,7 @@
 #' @param TARGET a vector of taxon information. To pass it must be non-empty
 #' entry. 
 #' 
-dc_validation_taxon_empty <- function(TARGET) {
+dc_validation_taxon_empty <- function(TARGET = NULL) {
   result <- TARGET %>%
     gsub(" ", "", .) # Remove possible spaces
   result <- !is.na(result) & result != "" # Must be a some string present
--- R/dc_validation_taxonid_empty.R
+++ R/dc_validation_taxonid_empty.R
@@ -3,7 +3,7 @@
 #' @param TARGET a vector of taxonid information. To pass it must be non-empty
 #' entry. 
 #' 
-dc_validation_taxonid_empty <- function(TARGET) {
+dc_validation_taxonid_empty <- function(TARGET = NULL) {
   result <- TARGET %>%
     gsub(" ", "", .) # Remove possible spaces
   result <- !is.na(result) & result != "" # Must be a some string present
