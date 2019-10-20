28eebbe6d7dad4f41aa8e61ac93608a0be8db728
PoGibas <povilasgibas@gmail.com>
2019-10-20 16:19:57 +0200

    v0.1.916 Adding event checks

---
+++ R/documentation_validation_eventdate_empty.R
@@ -0,0 +1,21 @@
+#'
+#' Data check validation_eventdate_empty Check if eventDate exists and is not empty.
+#'
+#'     This data check answers: "Is eventDate information present?" question.\cr Data check will pass if \strong{The value for eventDate is present.} and will fail if \strong{The value for eventDate is missing.}.\cr Dimension of this data check is \strong{} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{eventDate=2001-01-01}, such data check would return \code{TRUE}.\cr Example of entries that will fail: \code{eventDate=}, such data check would return \code{FALSE}.
+#' @name dc_validation_eventdate_empty
+#' @format An object of class function to perform a specific data check.
+#' @references None
+#' @examples 
+#' perform_dc(data_bats, 'validation_eventdate_empty')
+#' @section samplePassData:
+#' The value for eventDate is present.
+#' @section sampleFailData:
+#' The value for eventDate is missing.
+#' @section targetDWCField:
+#' eventDate
+#' @section checkCategory:
+#'  event
+#' @importFrom magrittr %>%
+#' @export
+#' @keywords event,validation,completness,missing,incomplete
+NULL
