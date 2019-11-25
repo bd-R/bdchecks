2548d41be0203518aea0955d4785684e45422e52
PoGibas <povilasgibas@gmail.com>
2019-11-25 19:07:55 +0200

    Added validation_basisofrecord_notstandard data check, but not tested

---
+++ R/dc_validation_basisofrecord_notstandard.R
@@ -0,0 +1,28 @@
+#' @rdname dc_validation_basisofrecord_notstandard
+#' 
+#' @param TARGET a vector of basisOfRecord information. To pass it must be
+#' within given reference dictionary.
+#' 
+dc_validation_basisofrecord_notstandard <- function(
+  TARGET = NULL,
+  sources = c(
+    "event",
+    "fossil",
+    "fossilspecimen",
+    "human",
+    "humanobservation",
+    "livingspecimen",
+    "machineobservation",
+    "materialsample",
+    "occurrence",
+    "preservedspecimen",
+    "taxon"
+  )
+) {
+  result <- TARGET %>%
+    gsub(" ", "", .) %>% # Remove possible spaces
+    gsub("_| |\\.|-", "", .) %>% # Remove seperators
+    tolower()
+  result <- result %in% sources
+  return(perform_dc_missing(result, TARGET))
+}
--- R/documentation_validation_kingdom_notfound.R
+++ R/documentation_validation_kingdom_notfound.R
@@ -1,7 +1,7 @@
 #'
 #' Data check validation_kingdom_notfound Check if kingdom was found as a value at the rank of kingdom by the specified source authority service.
 #'
-#'     This data check answers: "Is kingdom information present?" question.\cr Data check will pass if \strong{The value for kingdom is present and correct.} and will fail if \strong{The value for kingdom is missing or incorrect.}.\cr Dimension of this data check is \strong{} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{kingdom=Animalia}, such data check would return \code{TRUE}.\cr Example of entries that will fail: \code{phylum=birds}, such data check would return \code{FALSE}.
+#'     This data check answers: "Is kingdom information present?" question.\cr Data check will pass if \strong{The value for kingdom is present and correct.} and will fail if \strong{The value for kingdom is missing or incorrect.}.\cr Dimension of this data check is \strong{} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{kingdom=Animalia}, such data check would return \code{TRUE}.\cr Example of entries that will fail: \code{kingdom=birds}, such data check would return \code{FALSE}.
 #' @name dc_validation_kingdom_notfound
 #' @format An object of class function to perform a specific data check.
 #' @references None
