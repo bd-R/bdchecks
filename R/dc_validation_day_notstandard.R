2dc49f232cdd1dbf01ab9bf28181899549142906
PoGibas <povilasgibas@gmail.com>
2019-12-03 21:19:57 +0100

    Added validation_day_notstandard data check

---
+++ R/dc_validation_day_notstandard.R
@@ -0,0 +1,14 @@
+#' @rdname dc_validation_day_notstandard
+#' 
+#' @param TARGET a vector of day information. To pass must be an integer
+#' between 1 and 31.
+#' 
+dc_validation_day_notstandard <- function(TARGET = NULL) {
+  suppressWarnings(
+    result <- TARGET %>%
+      gsub(" ", "", .) %>% # Remove possible spaces
+      as.numeric()
+  )
+  result <- result %% 1 == 0 & result >= 1 & result <= 31
+  return(perform_dc_missing(result, TARGET))
+}
--- R/documentation_validation_month_notstandard.R
+++ R/documentation_validation_month_notstandard.R
@@ -1,7 +1,7 @@
 #'
-#' Data check validation_month_notstandard Check if month is an interget between 1 & 12.
+#' Data check validation_month_notstandard Check if month is an interget between 1 and 12 inclusive.
 #'
-#'     This data check answers: "Is month an integer between 1 & 12?" question.\cr Data check will pass if \strong{The value for month within given numbers.} and will fail if \strong{The value for month is not within given numbers.}.\cr Dimension of this data check is \strong{} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{month=6}, such data check would return \code{TRUE}.\cr Example of entries that will fail: \code{month=13}, such data check would return \code{FALSE}.
+#'     This data check answers: "Is month an integer between 1 and 12 inclusive?" question.\cr Data check will pass if \strong{The value for month within given numbers.} and will fail if \strong{The value for month is not within given numbers.}.\cr Dimension of this data check is \strong{} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{month=6}, such data check would return \code{TRUE}.\cr Example of entries that will fail: \code{month=13}, such data check would return \code{FALSE}.
 #' @name dc_validation_month_notstandard
 #' @format An object of class function to perform a specific data check.
 #' @references None
