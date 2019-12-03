29c0d73816ba4ea210815fa69cd4db42fd0ac739
PoGibas <povilasgibas@gmail.com>
2019-12-03 21:35:18 +0100

    Added validation_minelevation_outofrange data check

--- R/documentation_validation_day_notstandard.R
+++ R/documentation_validation_day_notstandard.R
@@ -1,5 +1,5 @@
 #'
-#' Data check validation_day_notstandard Check if day is an interget between 1 and 31 inclusive.
+#' Data check validation_day_notstandard Check if day is an integer between 1 and 31 inclusive.
 #'
 #'     This data check answers: "Is day an integer between 1 and 31 inclusive?" question.\cr Data check will pass if \strong{The value for day within given numbers.} and will fail if \strong{The value for day is not within given numbers.}.\cr Dimension of this data check is \strong{} and it's flagging type is: \strong{FLAG}\cr Example of entries that will pass: \code{day=6}, such data check would return \code{TRUE}.\cr Example of entries that will fail: \code{day=32}, such data check would return \code{FALSE}.
 #' @name dc_validation_day_notstandard
