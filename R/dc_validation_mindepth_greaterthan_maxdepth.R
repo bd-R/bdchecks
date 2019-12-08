524c6f0a1eb5c0a1b9790dcb541cc0ef65bc81f0
PoGibas <povilasgibas@gmail.com>
2019-12-08 14:40:10 +0100

    v0.1.111 Added validation_mindepth_greaterthan_maxdepth data check.

---
+++ R/dc_validation_mindepth_greaterthan_maxdepth.R
@@ -0,0 +1,24 @@
+#' @rdname dc_validation_mindepth_greaterthan_maxdepth
+#' 
+#' @param TARGET a vector of minimumDepthInMeters.
+#' @param TARGET2 a vector of maximumDepthInMeters.
+#' 
+dc_validation_mindepth_greaterthan_maxdepth <- function(
+  TARGET = NULL,
+  TARGET2 = NULL
+) {
+  suppressWarnings(
+    result1 <- TARGET %>%
+      trimws() %>%
+      as.numeric()
+  )
+  suppressWarnings(
+    result2 <- TARGET2 %>%
+      trimws() %>%
+      as.numeric()
+  )
+  result <- as.numeric(result1) <= as.numeric(result2)
+  foo <- perform_dc_missing(result, TARGET)
+  bar <- perform_dc_missing(result, TARGET2)
+  return(foo + bar == 2)
+}
