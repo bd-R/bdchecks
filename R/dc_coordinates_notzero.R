76868d6d20153b2330ddcd9d86682b0f980d5e8c
Martis6 <martjocys@gmail.com>
2020-06-04 12:51:58 +0300

    more checklogic changing

--- R/dc_coordinates_notzero.R
+++ R/dc_coordinates_notzero.R
@@ -1,12 +1,12 @@
 #' @rdname dc_coordinates_notzero
 #' 
 #' @param TARGET a vector of decimalLatitude or decimalLongitude information.
-#' To pass it must equal to zero.
+#' To pass it must not be equal to zero.
 #' 
 dc_coordinates_notzero <- function(TARGET = NULL) {
   # Supressing warnings if missing value is being turned into a numeric
   suppressWarnings(
-    result <- as.numeric(TARGET) == 0
+    result <- as.numeric(TARGET) != 0
   )
   return(perform_dc_missing(result, TARGET))
 }
